7164903299406f1efe2916d3bb5c0b213f66bbc8
tecnopulsar <tecnopulsar@gmail.com>
2023-04-24 14:11:26 -0300

    api clima material ui

---
+++ src/App.jsx
@@ -0,0 +1,116 @@
+import { LoadingButton } from "@mui/lab";
+import { Box, Container, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+export default function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({
+    city: "",
+    country: "",
+    temp: "",
+    condition: "",
+    icon: "",
+    conditionText: ",",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+
+  const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${
+    import.meta.env.VITE_API_KEY
+  }&lang=es&q=`;
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError({
+      error: false,
+      message: "",
+    });
+
+    try {
+      if (!city.trim()) throw { message: "El campo ciudad es obligatorio" };
+      const response = await fetch(`${API_WEATHER}${city}`);
+      const data = await response.json();
+      if (data.error) throw { message: data.error.message };
+      setWeather({
+        city: data.location.name,
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.code,
+        icon: data.current.condition.icon,
+        conditionText: data.current.condition.text,
+      });
+      console.log(weather);
+    } catch (error) {
+      setError({
+        error: true,
+        message: error.message,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Container maxWidth="xs" sx={{ mt: 2 }}>
+      <Typography variant="h3" component="h1" align="center" gutterBottom>
+        api Clima
+      </Typography>
+      <Box
+        sx={{ display: "grid", gap: 2 }}
+        component="form"
+        onSubmit={onSubmit}
+      >
+        <TextField
+          id="city"
+          label="Ciudad"
+          variant="outlined"
+          size="small"
+          required
+          fullWidth
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          error={error.error}
+          helperText={error.message}
+        />
+        <LoadingButton
+          type="submit"
+          variant="contained"
+          loading={loading}
+          loadingIndicator="Buscando ..."
+        >
+          Buscar datos del clima
+        </LoadingButton>
+      </Box>
+      {(city != '') ? (weather.city && (
+        <Box sx={{ mt: 2, display: "grid", gap: 2, textAling: "center" }}>
+          <Typography variant="h4" component="h2">
+            {weather.city}, {weather.country}
+          </Typography>
+          <Box
+            component="img"
+            alt={weather.conditionText}
+            src={weather.icon}
+            sx={{ margin: "0 auto" }}
+          />
+          <Typography variant="h5" component="h3" sx={{ margin: "0 auto" }}>
+            {weather.temp} C
+          </Typography>
+          <Typography variant="h6" component="h4" sx={{ margin: "0 auto" }}>
+            {weather.conditionText}
+          </Typography>
+        </Box>
+      )):null}
+
+      <Typography textAlign="center" sx={{ mt: 2, fontSize: "12px" }}>
+        Power by:{" "}
+        <a href="https://www.weatherapi.com/" title="weather API">
+          WeatherAPI.com
+        </a>
+      </Typography>
+    </Container>
+  );
+}
